02e30150caaac8583538599b8397262b853160bb
Nishanth D Aluhonnu <nishanthdilip@yahoo.co.in>
2018-07-04 03:25:27 +0530

    Update test_discord_bot.py

--- test_discord_bot.py
+++ test_discord_bot.py
@@ -67,6 +67,74 @@ async def on_message(message):
         await client.send_message(message.channel, o1)
         await client.send_message(message.channel, o2)
         await client.send_message(message.channel, o3)
+
+        tab="https://www.bing.com/search?q="
+        tabb="https://search.aol.com/aol/search?s_chn=prt_bon&q=" 
+        tab1="https://search.aol.com/aol/search?s_chn=prt_bon&q=" #num=30&ei=py29WruLGoiSvQSbsIjYAg&
+        tab2="https://www.google.co.in/search?q="        
+        
+        response=requests.get(tab+q+"&count=30")        
+        soup=BeautifulSoup(response.text,"html.parser")        
+        response1=requests.get(tabb+q)        
+        soup1=BeautifulSoup(response1.text,"html.parser")
+        #print(soup1)
+        
+        links = soup1.findAll("a")
+        #print (links)
+        ctrrrr=0
+        for link in  soup1.find_all("a"):
+            ctrrrr+=1
+            firstlink=re.split(":(?=http)",link["href"].replace("/url?q=",""))
+            #print(firstlink)
+            if(ctrrrr==19):              
+                break
+                
+            
+           # print (firstlink)
+        
+        
+            
+        firstlink[0]=firstlink[0][0:]
+        #print(firstlink[0])
+        
+        response2=requests.get(firstlink[0])        
+        soup2=BeautifulSoup(response2.text,"html.parser")
+            
+            #break
+##        for anchor in soup.findAll('a', href=True):
+##            print anchor['href']
+                
+        ss1=soup.get_text().lower()
+        #print (ss1)
+        ss2=soup1.get_text().lower()
+        #print (ss2)
+        ss3=soup2.get_text().lower()
+##        ss15=ss1.split(" resultsdate language region",1)[0]
+##        ss1=ss1.split(" resultsdate language region",1)[1]
+        #print(ss1)
+        ss1=ss1.replace(" ","")
+        ss2=ss2.replace(" ","")
+        ss3=ss3.replace(" ","")
+        #print (ss3)
+        ca=ss3.count(o1.lower().replace(" ",""))       
+        cb=ss3.count(o2.lower().replace(" ",""))
+        cc=ss3.count(o3.lower().replace(" ",""))        
+        c1= ss1.count(o1.lower().replace(" ","")) + ss2.count(o1.lower().replace(" ","")) + ca 
+        c2=  ss1.count(o2.lower().replace(" ","")) + ss2.count(o2.lower().replace(" ","")) + cb
+        c3=   ss1.count(o3.lower().replace(" ","")) + ss2.count(o3.lower().replace(" ","")) + cc
+        
+        
+        m=max(c1,c2,c3)
+        await client.send_message(message.channel, o1+"       "+str(c1)+"           "+str(ca))
+        await client.send_message(message.channel, o2+"       "+str(c2)+"           "+str(cb))
+        await client.send_message(message.channel, o3+"       "+str(c3)+"           "+str(cc))
+        
+        if m==c1:
+            await client.send_message(message.channel, "\n"+"111   "+o1+" "+o1+" "+o1+" "+o1)
+        elif m==c2:
+            await client.send_message(message.channel, "\n"+"111   "+o2+" "+o2+" "+o2+" "+o2)
+        elif m==c3:
+            await client.send_message(message.channel, "\n"+"111   "+o3+" "+o3+" "+o3+" "+o3)
             
   
                     
