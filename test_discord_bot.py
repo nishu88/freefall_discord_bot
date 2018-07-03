ecc4af93e2231b339b2bd98b4b8b8fb62636f3b9
Nishanth D Aluhonnu <nishanthdilip@yahoo.co.in>
2018-07-04 01:25:40 +0530

    Update test_discord_bot.py

--- test_discord_bot.py
+++ test_discord_bot.py
@@ -21,31 +21,30 @@ async def on_ready():
 ##    if message.content == "cookie":
 ##        await client.send_message(message.channel, ":cookie:") #responds with Cookie emoji when someone says "cookie"
 
-##@client.event
-##async def on_message(message):
-##    if message.content.upper().startswith('!PING'):
-##        userID = message.author.id
-##        await client.send_message(message.channel, "<@%s> Pong!" % (userID))
-##    if message.content.upper().startswith('!SAY'):
-##        args = message.content.split(" ")
-##        #args[0] = !SAY
-##        #args[1] = Hey
-##        #args[2] = There
-##        #args[1:] = Hey There
-##        await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
-
 @client.event
 async def on_message(message):
-    contents = message.content.split(" ") #contents is a list type
-    for word in contents:
-        if word.upper() in chat_filter:
-            if not message.author.id in bypass_list:
-                try:
-                    await client.delete_message(message)
-                    await client.send_message(message.channel, "**Hey!** You're not allowed to use that word here!")
-                except discord.errors.NotFound:
-                    return
-                   
+    if message.content.upper().startswith('!PING'):
+        userID = message.author.id
+        await client.send_message(message.channel, "<@%s> Pong!" % (userID))
+    if message.content.upper().startswith('!SAY'):
+        args = message.content.split(" ")
+        #args[0] = !SAY
+        #args[1] = Hey
+        #args[2] = There
+        #args[1:] = Hey There
+        await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
+
+##@client.event
+##async def on_message(message):
+##    contents = message.content.split(" ") #contents is a list type
+##    for word in contents:
+##        if word.upper() in chat_filter:
+##            if not message.author.id in bypass_list:
+##                try:
+##                    await client.delete_message(message)
+##                    await client.send_message(message.channel, "**Hey!** You're not allowed to use that word here!")
+##                except discord.errors.NotFound:
+##                    return
                     
         
 
