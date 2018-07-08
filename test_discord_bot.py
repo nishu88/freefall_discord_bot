62cc5902a817e44510360b432d1613774155e0b6
Nishanth D Aluhonnu <nishanthdilip@yahoo.co.in>
2018-07-08 19:52:36 +0530

    Update test_discord_bot.py

--- test_discord_bot.py
+++ test_discord_bot.py
@@ -185,10 +185,8 @@ async def on_message(message):
         #await client.send_message(message.channel, o2+"       "+str(c2)+"           "+str(cb))
         #await client.send_message(message.channel, o3+"       "+str(c3)+"           "+str(cc))
         
-        if m==c1:
-            asdfgh=str(message.channel)
-            await client.send_message(message.channel,"\n"+"Best Possibility=  "+"111   "+o1)
-            await client.send_message(message.channel,asdfgh)
+        if m==c1:            
+            await client.send_message(message.channel,"\n"+"Best Possibility=  "+"111   "+o1)            
             #await client.send_message(discord.Object(id='464836410373832704'), 'hello')
             if(m==0):
                 await client.send_message(discord.Object(id='284994155443126273'),"Sorry, But you're on ur own Mate")   
